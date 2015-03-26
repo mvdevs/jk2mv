52c406600553536bce5e4b6513748dcc8d732e72
ouned <michael@losert.org>
2015-03-26 23:21:43 +0100

    fix for issue #2 not uppercasing demo filenames anymore

--- src/menu/ui_main.c
+++ src/menu/ui_main.c
@@ -3752,10 +3752,6 @@ static void UI_LoadDemos() {
 			demoname = demolist;
 			for ( i = oldCount; i < uiInfo.demoCount; i++ ) {
 				len = strlen( demoname );
-				/*if (!Q_stricmp(demoname +  len - strlen(demoExt), demoExt)) {
-					demoname[len-strlen(demoExt)] = '\0';
-				}*/
-				Q_strupr(demoname);
 				uiInfo.demoList[i] = String_Alloc(demoname);
 				demoname += len + 1;
 			}
