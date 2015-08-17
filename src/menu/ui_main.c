mvdevs/jk2mv

commit 819a5600ed2aa4715c2c0cdd7cea9abd92f47cb7

VS2010 fixes.

src/menu/ui_main.c
@@ -5040,14 +5040,14 @@ static void UI_BuildServerDisplayList(qboolean force) {
 		// get the ping for this server
 		ping = trap_LAN_GetServerPing(ui_netSource.integer, i);
 		if (ping > 0 || ui_netSource.integer == AS_FAVORITES) {
-
-			trap_LAN_GetServerInfo(ui_netSource.integer, i, info, MAX_STRING_CHARS);
-
 			// ouned: botfilter
 			int clients = atoi(Info_ValueForKey(info, "clients"));
 			int bots = atoi(Info_ValueForKey(info, "bots"));
 
 			int realPlayers;
+
+			trap_LAN_GetServerInfo(ui_netSource.integer, i, info, MAX_STRING_CHARS);
+
 			if (trap_Cvar_VariableValue("ui_botfilter")) {
 				realPlayers = clients - bots;
 			} else {

src/sys/sys_main.cpp
@@ -4,7 +4,7 @@
 #include <cstdio>
 #include <sys/stat.h>
 #define __STDC_FORMAT_MACROS
-#include <inttypes.h>
+#include <stdint.h>
 #include "../qcommon/qcommon.h"
 #include "sys_local.h"
 #include "con_local.h"

commit 72be8b295cd76bdad590c64f4a7d9f41b7b6ff18

Fixed the botfilter (broken in 819a5600ed2aa4715c2c0cdd7cea9abd92f47cb7).

src/menu/ui_main.c
@@ -5041,12 +5041,14 @@ static void UI_BuildServerDisplayList(qboolean force) {
 		ping = trap_LAN_GetServerPing(ui_netSource.integer, i);
 		if (ping > 0 || ui_netSource.integer == AS_FAVORITES) {
 			// ouned: botfilter
-			int clients = atoi(Info_ValueForKey(info, "clients"));
-			int bots = atoi(Info_ValueForKey(info, "bots"));
-
 			int realPlayers;
+			int clients;
+			int bots;
 
 			trap_LAN_GetServerInfo(ui_netSource.integer, i, info, MAX_STRING_CHARS);
+
+			clients = atoi(Info_ValueForKey(info, "clients"));
+			bots = atoi(Info_ValueForKey(info, "bots"));
 
 			if (trap_Cvar_VariableValue("ui_botfilter")) {
 				realPlayers = clients - bots;