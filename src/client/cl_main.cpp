mvdevs/jk2mv

commit 53ad31d93ea26e642270204c9cb1d1dc4e5b377c

external url support for http downloads close #16

src/client/cl_main.cpp
@@ -1493,7 +1493,7 @@ void CL_BeginDownload( const char *localName, const char *remoteName ) {
 	Cvar_SetValue("cl_downloadCount", 0);
 	Cvar_SetValue("cl_downloadTime", 0);
 
-	if (clc.http_port != 0 && clc.serverAddress.type == NA_IP) {
+	if (clc.httpdl[0]) {
 		// downloading over http has priority
 		Cvar_Set("cl_downloadProtocol", "HTTP");
 	} else {
@@ -1532,8 +1532,7 @@ void CL_ContinueCurrentDownload(qboolean abort) {
 	if (!Q_stricmp(cl_downloadProtocol->string, "HTTP")) {
 		char remotepath[MAX_STRING_CHARS];
 
-		Q_strncpyz(remotepath, va("http://%i.%i.%i.%i:%i/%s", clc.serverAddress.ip[0], clc.serverAddress.ip[1], clc.serverAddress.ip[2], clc.serverAddress.ip[3],
-			clc.http_port, cl_downloadName->string), sizeof(remotepath));
+		Q_strncpyz(remotepath, va("%s/%s", clc.httpdl, cl_downloadName->string), sizeof(remotepath));
 		Com_DPrintf("HTTP URL: %s\n", remotepath);
 
 		cls.curl = curl_easy_init();
@@ -1638,7 +1637,7 @@ void CL_InitDownloads(void) {
 	} else if ( FS_ComparePaks( clc.downloadList, sizeof( clc.downloadList ) , qtrue ) ) {
 		Com_Printf("Need paks: %s\n", clc.downloadList );
 
-		if (*clc.downloadList && (clc.udpdl || clc.http_port)) {
+		if (*clc.downloadList && (clc.udpdl || clc.httpdl[0])) {
 			cls.state = CA_CONNECTED;
 
 			// to get the download popup jk2mvmenu must be loaded instaed of the normal ui
@@ -1693,7 +1692,8 @@ void CL_CheckForResend( void ) {
 			CL_RequestAuthorization();
 		}
 #endif	// USE_CD_KEY
-		clc.http_port = -1;
+		clc.httpdl[0] = 0;
+		clc.httpdlvalid = qfalse;
 		clc.udpdl = -1;
 #ifdef MV_MFDOWNLOADS
 		NET_OutOfBandPrint(NS_CLIENT, clc.serverAddress, "jk2mfport");
@@ -1704,7 +1704,7 @@ void CL_CheckForResend( void ) {
 		break;
 
 	case CA_CHALLENGING:
-		if (MV_GetCurrentGameversion() == VERSION_UNDEF || clc.http_port == -1 || clc.udpdl == -1)
+		if (MV_GetCurrentGameversion() == VERSION_UNDEF || !clc.httpdlvalid || clc.udpdl == -1)
 			break;
 
 		// sending back the challenge
@@ -2159,9 +2159,8 @@ void CL_ConnectionlessPacket( netadr_t from, msg_t *msg ) {
 	}
 
 #ifdef MV_MFDOWNLOADS
-	int mfport = atoi(c);
-	if (cls.state == CA_CONNECTING && NET_CompareAdr(from, clc.serverAddress) && mfport > 0 && clc.http_port == -1) {
-		clc.http_port = mfport;
+	if (strtol(c, NULL, 10) && cls.state == CA_CONNECTING && NET_CompareAdr(from, clc.serverAddress)) {
+		Q_strncpyz(clc.httpdl, va("http://%i.%i.%i.%i:%s", clc.serverAddress.ip[0], clc.serverAddress.ip[1], clc.serverAddress.ip[2], clc.serverAddress.ip[3], c), sizeof(clc.httpdl));
 
 		return;
 	}
@@ -3020,8 +3019,25 @@ void CL_ServerInfoPacket( netadr_t from, msg_t *msg ) {
 			}
 		}
 
-		if (clc.http_port == -1)
-			clc.http_port = atoi(Info_ValueForKey(infoString, "mvhttp"));
+		if (!clc.httpdl[0]) {
+			char *val;
+			
+			val = Info_ValueForKey(infoString, "mvhttp");
+			if (strtol(val, NULL, 10)) {
+				Q_strncpyz(clc.httpdl, va("http://%i.%i.%i.%i:%s", clc.serverAddress.ip[0], clc.serverAddress.ip[1], clc.serverAddress.ip[2], clc.serverAddress.ip[3], val), sizeof(clc.httpdl));
+			} else if ((val = Info_ValueForKey(infoString, "mvhttpurl")) && Q_stristr(val, "http://")) {
+				Q_strncpyz(clc.httpdl, val, sizeof(clc.httpdl));
+
+				// make sure there is no "/" on the end
+				// so it always is in the format "http://a.org"
+				size_t len = strlen(clc.httpdl);
+				if (clc.httpdl[len - 1] == '/') {
+					clc.httpdl[len - 1] = 0;
+				}
+			}
+
+			clc.httpdlvalid = qtrue;
+		}
 
 		return;
 	}

src/client/client.h
@@ -214,7 +214,8 @@ typedef struct {
 	char		downloadList[MAX_INFO_STRING]; // list of paks we need to download
 	qboolean	downloadRestart;	// if true, we need to do another FS_Restart because we downloaded a pak
 
-	int http_port;
+	char httpdl[128];
+	qboolean httpdlvalid;
 	int udpdl;
 
 	// demo information

src/qcommon/cvar.cpp
@@ -559,7 +559,7 @@ static void Cvar_PrintFlags (cvar_t* cv) {
 				Q_strcat(buf, sizeof(buf), va("%s, ", cvarflags[i].s));
 		}
 
-		int ln = strlen( buf );
+		size_t ln = strlen( buf );
 		if (ln && buf[ln - 1] == ' ')
 			buf[ln - 2] = 0;	//cut off ", "
 	}

src/server/sv_init.cpp
@@ -473,7 +473,7 @@ void *SV_MV_Websrv_Loop_ExtThread(void *server) {
 }
 
 void SV_MV_Websrv_Shutdown() {
-	Com_Printf("shutting down webserver...\n");
+	Com_Printf("HTTP Downloads: shutting down webserver...\n");
 
 	MV_LockMutex(m_srv);
 	m_end = qtrue;
@@ -800,33 +800,37 @@ Ghoul2 Insert End
 	mv_httpserverport = Cvar_Get("mv_httpserverport", "0", CVAR_ARCHIVE | CVAR_LATCH);
 
 	// start webserver
-	if (!mgsrv && mv_httpdownloads->integer) {
-		const char *err = NULL;
-		int port;
+	if (mv_httpdownloads->integer) {
+		if (Q_stristr(mv_httpserverport->string, "http://")) {
+			Com_Printf("HTTP Downloads: redirecting to %s\n", mv_httpserverport->string);
+		} else if (!mgsrv) {
+			const char *err = NULL;
+			int port;
 
-		mgsrv = mg_create_server(NULL, SV_MV_Websrv_Request_ExtThread);
-		mg_set_option(mgsrv, "document_root", Cvar_Get("fs_basepath", "", 0)->string);
+			mgsrv = mg_create_server(NULL, SV_MV_Websrv_Request_ExtThread);
+			mg_set_option(mgsrv, "document_root", Cvar_Get("fs_basepath", "", 0)->string);
 
-		if (mv_httpserverport->integer) {
-			port = mv_httpserverport->integer;
-			err = mg_set_option(mgsrv, "listening_port", va("%i", port));
-		} else {
-			for (port = HTTPSRV_STDPORT; port <= HTTPSRV_STDPORT + 15; port++) {
+			if (mv_httpserverport->integer) {
+				port = mv_httpserverport->integer;
 				err = mg_set_option(mgsrv, "listening_port", va("%i", port));
-				if (!err) {
-					break;
+			} else {
+				for (port = HTTPSRV_STDPORT; port <= HTTPSRV_STDPORT + 15; port++) {
+					err = mg_set_option(mgsrv, "listening_port", va("%i", port));
+					if (!err) {
+						break;
+					}
 				}
 			}
-		}
 
-		if (!err) {
-			sv.http_port = port;
-			Com_Printf("webserver running on port %i...\n", port);
-		}  else {
-			Com_Error(ERR_DROP, "webserver startup failed: %s", err);
-		}
+			if (!err) {
+				sv.http_port = port;
+				Com_Printf("HTTP Downloads: webserver running on port %i...\n", port);
+			} else {
+				Com_Error(ERR_DROP, "HTTP Downloads: webserver startup failed: %s", err);
+			}
 
-		mg_start_thread(SV_MV_Websrv_Loop_ExtThread, mgsrv);
+			mg_start_thread(SV_MV_Websrv_Loop_ExtThread, mgsrv);
+		}
 	}
 }
 

src/server/sv_main.cpp
@@ -595,7 +595,11 @@ void SVC_Info( netadr_t from ) {
 
 	// webserver port
 	if (mv_httpdownloads->integer) {
-		Info_SetValueForKey(infostring, "mvhttp", va("%i", sv.http_port));
+		if (Q_stristr(mv_httpserverport->string, "http://")) {
+			Info_SetValueForKey(infostring, "mvhttpurl", mv_httpserverport->string);
+		} else {
+			Info_SetValueForKey(infostring, "mvhttp", va("%i", sv.http_port));
+		}
 	}
 
 	NET_OutOfBandPrint( NS_SERVER, from, "infoResponse\n%s", infostring );

commit 97ef387f7b851e97b20ab0f2db184631fc4fc7e2

fixed creating non dedicated server bug introduced in 53ad31d93ea26e642270204c9cb1d1dc4e5b377c

src/client/cl_main.cpp
@@ -1802,7 +1802,7 @@ void CL_CheckForResend( void ) {
 		break;
 
 	case CA_CHALLENGING:
-		if (MV_GetCurrentGameversion() == VERSION_UNDEF || !clc.httpdlvalid || clc.udpdl == -1)
+		if (MV_GetCurrentGameversion() == VERSION_UNDEF || ( ( !clc.httpdlvalid || clc.udpdl == -1 ) && com_dedicated->integer) )
 			break;
 
 		// sending back the challenge

src/client/client.h
@@ -15,7 +15,7 @@
 #	include <curl/curl.h>
 #endif
 
-#define	RETRANSMIT_TIMEOUT	5000	// time between connection packet retransmits
+#define	RETRANSMIT_TIMEOUT	3000	// time between connection packet retransmits
 
 // Wind
 extern vec3_t cl_windVec;