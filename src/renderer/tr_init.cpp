mvdevs/jk2mv

commit 7647aebcb0fe017dd7bf05c7c29782dbdb84e3bc

r_overbrightbits support for postprocessing gamma method

src/renderer/tr_init.cpp
@@ -262,6 +262,9 @@ PFNGLGETPROGRAMIVARBPROC qglGetProgramivARB = NULL;
 PFNGLGETPROGRAMSTRINGARBPROC qglGetProgramStringARB = NULL;
 PFNGLISPROGRAMARBPROC qglIsProgramARB = NULL;
 
+PFNGLTEXIMAGE3DPROC qglTexImage3D;
+PFNGLTEXSUBIMAGE3DPROC qglTexSubImage3D;
+
 #endif
 
 void RE_SetLightStyle(int style, int color);
@@ -1312,8 +1315,8 @@ void RE_Shutdown( qboolean destroyWindow ) {
 		qglDeleteProgramsARB(1, &tr.gammaVertexShader);
 	}
 
-	if (tr.gammaRenderTarget) {
-		qglDeleteTextures(1, &tr.gammaRenderTarget);
+	if (tr.gammaLUTImage) {
+		qglDeleteTextures(1, &tr.gammaLUTImage);
 	}
 	// --------
 

src/game/q_shared.c
@@ -59,6 +59,15 @@ float Com_Clamp( float min, float max, float value ) {
 	return value;
 }
 
+int Com_Clampi(int min, int max, int value) {
+	if (value < min) {
+		return min;
+	}
+	if (value > max) {
+		return max;
+	}
+	return value;
+}
 
 /*
 ============

src/game/q_shared.h
@@ -886,6 +886,7 @@ void PerpendicularVector( vec3_t dst, const vec3_t src );
 //=============================================
 
 float Com_Clamp( float min, float max, float value );
+int Com_Clampi(int min, int max, int value);
 
 char	*COM_SkipPath( char *pathname );
 void	COM_StripExtension(const char *in, char *out, int destsize);

src/renderer/qgl.h
@@ -119,6 +119,10 @@ typedef void (APIENTRY * PFNGLCLIENTACTIVETEXTUREARBPROC) (GLenum target);
 //	- Define the function pointer in tr_init.cpp and possibly add a cvar to track your ext status.
 //	- Load the extension in win_glimp.cpp.
 
+typedef void (APIENTRY * PFNGLTEXIMAGE3DPROC) (GLenum target, GLint level, GLint internalformat, GLsizei width, GLsizei height, GLsizei depth, GLint border, GLenum format, GLenum type, const GLvoid *pixels);
+typedef void (APIENTRY * PFNGLTEXSUBIMAGE3DPROC) (GLenum target, GLint level, GLint xoffset, GLint yoffset, GLint zoffset, GLsizei width, GLsizei height, GLsizei depth, GLenum format, GLenum type, const GLvoid *pixels);
+extern PFNGLTEXIMAGE3DPROC qglTexImage3D;
+extern PFNGLTEXSUBIMAGE3DPROC qglTexSubImage3D;
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Register Combiner extension definitions. - AReis

src/renderer/tr_backend.cpp
@@ -1,4 +1,5 @@
 #include "tr_local.h"
+#include "glext.h"
 
 #ifndef DEDICATED
 #if !defined __TR_WORLDEFFECTS_H
@@ -1265,45 +1266,49 @@ const void	*RB_SwapBuffers( const void *data ) {
 
 	// gamma correction
 	if (glConfig.deviceSupportsPostprocessingGamma && r_gammamethod->integer == GAMMA_POSTPROCESSING) {
-		qglMatrixMode(GL_PROJECTION);
-		qglLoadIdentity();
-		qglOrtho(0, glConfig.vidWidth, glConfig.vidHeight, 0, 0, 1);
-		qglMatrixMode(GL_MODELVIEW);
-		qglLoadIdentity();
-		GL_State(GLS_DEPTHTEST_DISABLE);
+		RB_SetGL2D();
 
 		qglEnable(GL_VERTEX_PROGRAM_ARB);
 		qglBindProgramARB(GL_VERTEX_PROGRAM_ARB, tr.gammaVertexShader);
 		qglEnable(GL_FRAGMENT_PROGRAM_ARB);
 		qglBindProgramARB(GL_FRAGMENT_PROGRAM_ARB, tr.gammaPixelShader);
 
-		qglProgramEnvParameter4fARB(GL_FRAGMENT_PROGRAM_ARB, 0, 1 / r_gamma->value, 0.0f, 0.0f, 0.0f);
-
-		qglDisable(GL_TEXTURE_2D);
+		GL_SelectTexture(0);
 		qglEnable(GL_TEXTURE_RECTANGLE_EXT);
-		qglBindTexture(GL_TEXTURE_RECTANGLE_EXT, tr.gammaRenderTarget);
-		qglCopyTexSubImage2D(GL_TEXTURE_RECTANGLE_EXT, 0, 0, 0, 0, 0, glConfig.vidWidth, glConfig.vidHeight);
+		qglBindTexture(GL_TEXTURE_RECTANGLE_EXT, tr.sceneImage);
+		qglCopyTexImage2D(GL_TEXTURE_RECTANGLE_EXT, 0, GL_RGBA, 0, 0, glConfig.vidWidth, glConfig.vidHeight, 0);
 		qglDisable(GL_TEXTURE_RECTANGLE_EXT);
-		qglEnable(GL_TEXTURE_2D);
+
+		GL_SelectTexture(1);
+		qglEnable(GL_TEXTURE_3D);
+		qglBindTexture(GL_TEXTURE_3D, tr.gammaLUTImage);
+
+		qglClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+		qglClear(GL_COLOR_BUFFER_BIT);
 
 		qglBegin(GL_QUADS);
-			qglColor4f(0.0f, 0.0f, 0.0f, 0.0f);
-			qglTexCoord2f(0, glConfig.vidHeight);
-			qglVertex2f(0, 0);
-			qglTexCoord2f(0, 0);
-			qglVertex2f(0, glConfig.vidHeight);
-			qglTexCoord2f(glConfig.vidWidth, 0);
-			qglVertex2f(glConfig.vidWidth, glConfig.vidHeight);
-			qglTexCoord2f(glConfig.vidWidth, glConfig.vidHeight);
-			qglVertex2f(glConfig.vidWidth, 0);
+			qglTexCoord2f(0.0f, 0.0f);
+			qglVertex2f(-1.0f, -1.0f);
+
+			qglTexCoord2f(0.0f, (float)glConfig.vidHeight);
+			qglVertex2f(-1.0f, 1.0f);
+
+			qglTexCoord2f((float)glConfig.vidWidth, (float)glConfig.vidHeight);
+			qglVertex2f(1.0f, 1.0f);
+
+			qglTexCoord2f((float)glConfig.vidWidth, 0.0f);
+			qglVertex2f(1.0f, -1.0f);
 		qglEnd();
 
 		qglDisable(GL_VERTEX_PROGRAM_ARB);
 		qglDisable(GL_FRAGMENT_PROGRAM_ARB);
+
+		qglDisable(GL_TEXTURE_3D);
+		GL_SelectTexture(0);
 	}
 
-    // texture swapping test
-	if ( r_showImages->integer ) {
+	// texture swapping test
+	if (r_showImages->integer) {
 		RB_ShowImages();
 	}
 

src/renderer/tr_image.cpp
@@ -2401,19 +2401,6 @@ void R_CreateBuiltinImages( void ) {
 		R_CreateDlightImage();
 	}
 	R_CreateFogImage();
-
-	// gamma render target
-	if (glConfig.deviceSupportsPostprocessingGamma && r_gammamethod->integer == GAMMA_POSTPROCESSING) {
-		tr.gammaRenderTarget = 1024 + giTextureBindNum++;
-		qglEnable(GL_TEXTURE_RECTANGLE_EXT);
-		qglBindTexture(GL_TEXTURE_RECTANGLE_EXT, tr.gammaRenderTarget);
-		qglTexImage2D(GL_TEXTURE_RECTANGLE_EXT, 0, GL_RGB, glConfig.vidWidth, glConfig.vidHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
-		qglTexParameteri(GL_TEXTURE_RECTANGLE_EXT, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-		qglTexParameteri(GL_TEXTURE_RECTANGLE_EXT, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		qglTexParameteri(GL_TEXTURE_RECTANGLE_EXT, GL_TEXTURE_WRAP_S, GL_CLAMP);
-		qglTexParameteri(GL_TEXTURE_RECTANGLE_EXT, GL_TEXTURE_WRAP_T, GL_CLAMP);
-		qglDisable(GL_TEXTURE_RECTANGLE_EXT);
-	}
 }
 
 
@@ -2431,13 +2418,7 @@ void R_SetColorMappings( void ) {
 	// setup the overbright lighting
 	tr.overbrightBits = r_overBrightBits->integer;
 
-	// gamma correction
-	if (!glConfig.deviceSupportsGamma && !glConfig.deviceSupportsPostprocessingGamma) {
-		tr.overbrightBits = 0;		// need hardware gamma for overbright
-	}
-
-	// never overbright in windowed mode or when postprocessing
-	if (!glConfig.isFullscreen || r_gammamethod->integer == GAMMA_POSTPROCESSING)
+	if (!glConfig.isFullscreen && r_gammamethod->integer != GAMMA_POSTPROCESSING)
 	{
 		tr.overbrightBits = 0;
 	}
@@ -2465,23 +2446,54 @@ void R_SetColorMappings( void ) {
 	}
 
 	g = r_gamma->value;
-
 	shift = tr.overbrightBits;
 
-	for ( i = 0; i < 256; i++ ) {
-		if ( g == 1 ) {
-			inf = i;
-		} else {
-			inf = 255 * pow ( i/255.0f, 1.0f / g ) + 0.5f;
+	if (r_gammamethod->integer != GAMMA_POSTPROCESSING) {
+		for (i = 0; i < 256; i++) {
+			if (g == 1) {
+				inf = i;
+			} else {
+				inf = 255 * pow(i / 255.0f, 1.0f / g) + 0.5f;
+			}
+			inf <<= shift;
+			if (inf < 0) {
+				inf = 0;
+			}
+			if (inf > 255) {
+				inf = 255;
+			}
+			s_gammatable[i] = inf;
 		}
-		inf <<= shift;
-		if (inf < 0) {
-			inf = 0;
+	} else {
+		byte gammaCorrected[64];
+		
+		for (int i = 0; i < 64; i++) {
+			if (g == 1.0f) {
+				inf = (int)(((float)i / 63.0f) * 255.0f + 0.5f);
+			} else {
+				inf = (int)(255.0f * pow((float)i / 63.0f, 1.0f / g) + 0.5f);
+			}
+
+			gammaCorrected[i] = Com_Clampi(0, 255, inf << shift);
 		}
-		if (inf > 255) {
-			inf = 255;
+		
+		byte *lutTable = (byte *)Hunk_AllocateTempMemory(64 * 64 * 64 * 3);
+		byte *write = lutTable;
+		for (int z = 0; z < 64; z++) {
+			for (int y = 0; y < 64; y++) {
+				for (int x = 0; x < 64; x++) {
+					*write++ = gammaCorrected[x];
+					*write++ = gammaCorrected[y];
+					*write++ = gammaCorrected[z];
+				}
+			}
 		}
-		s_gammatable[i] = inf;
+
+		qglBindTexture(GL_TEXTURE_3D, tr.gammaLUTImage);
+		qglPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+		qglTexSubImage3D(GL_TEXTURE_3D, 0, 0, 0, 0, 64, 64, 64, GL_RGB, GL_UNSIGNED_BYTE, lutTable);
+		
+		Hunk_FreeTempMemory(lutTable);
 	}
 
 	for (i=0 ; i<256 ; i++) {
@@ -2504,7 +2516,21 @@ R_InitImages
 ===============
 */
 void	R_InitImages( void ) {
-	//memset(hashTable, 0, sizeof(hashTable));	// DO NOT DO THIS NOW (because of image cacheing)	-ste.
+	// gamma render target
+	if (glConfig.deviceSupportsPostprocessingGamma && r_gammamethod->integer == GAMMA_POSTPROCESSING) {
+		qglEnable(GL_TEXTURE_3D);
+		tr.gammaLUTImage = 1024 + giTextureBindNum++;
+		qglBindTexture(GL_TEXTURE_3D, tr.gammaLUTImage);
+		qglTexImage3D(GL_TEXTURE_3D, 0, GL_RGBA8, 64, 64, 64, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+		qglTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		qglTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		qglTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		qglTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+		qglTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		qglDisable(GL_TEXTURE_3D);
+		qglEnable(GL_TEXTURE_2D);
+	}
+
 	// build brightness translation tables
 	R_SetColorMappings();
 

src/renderer/tr_local.h
@@ -1117,7 +1117,7 @@ typedef struct {
 
 	// gamma correction
 	GLuint gammaVertexShader, gammaPixelShader;
-	GLuint gammaRenderTarget;
+	GLuint gammaLUTImage;
 } trGlobals_t;
 
 

src/renderer/tr_shader.cpp
@@ -4055,23 +4055,17 @@ MV_GammaGenerateProgram
 #ifndef DEDICATED
 const char *g_GammaVertexShaderARB = {
 	"!!ARBvp1.0" "\n"
-	"DP4 result.position.x, state.matrix.mvp.row[0], vertex.position;" "\n"
-	"DP4 result.position.y, state.matrix.mvp.row[1], vertex.position;" "\n"
-	"DP4 result.position.z, state.matrix.mvp.row[2], vertex.position;" "\n"
-	"DP4 result.position.w, state.matrix.mvp.row[3], vertex.position;" "\n"
-	"MOV result.texcoord[0], vertex.texcoord;" "\n"
+	"MOV result.position, vertex.position;" "\n"
+	"MOV result.texcoord[0], vertex.texcoord[0];" "\n"
 	"END"
 };
 
 const char *g_GammaPixelShaderARB = {
 	"!!ARBfp1.0" "\n"
-	"PARAM c[1] = { program.env[0] };" "\n"
-	"TEMP R0;" "\n"
+	"TEMP r0;" "\n"
 
-	"TEX R0.xyz, fragment.texcoord[0], texture[0], RECT;" "\n"
-	"POW result.color.x, R0.x, c[0].x;" "\n"
-	"POW result.color.y, R0.y, c[0].x;" "\n"
-	"POW result.color.z, R0.z, c[0].x;" "\n"
+	"TEX r0.xyz, fragment.texcoord[0], texture[0], RECT;" "\n"
+	"TEX result.color, r0, texture[1], 3D;" "\n"
 	"END"
 };
 

commit d8d40924e7049937512c4c4c5c80b330b821a50f

fixed taking screenshots problem introduced in 7647aebcb0fe017dd7bf05c7c29782dbdb84e3bc close #42

src/renderer/tr_init.cpp
@@ -561,7 +561,7 @@ void R_TakeScreenshot(int x, int y, int width, int height, char *fileName) {
 	memcount = linelen * height;
 
 	// gamma correct
-	if (glConfig.deviceSupportsGamma)
+	if (r_gammamethod->integer == GAMMA_HARDWARE)
 		R_GammaCorrect(allbuf + offset, (int)memcount);
 
 	ri.FS_WriteFile(fileName, buffer, (int)memcount + 18);
@@ -578,7 +578,7 @@ void R_TakeScreenshotJPEG(int x, int y, int width, int height, char *fileName) {
 	memcount = (width * 3 + padlen) * height;
 
 	// gamma correct
-	if (glConfig.deviceSupportsGamma)
+	if (r_gammamethod->integer == GAMMA_HARDWARE)
 		R_GammaCorrect(buffer + offset, (int)memcount);
 
 	SaveJPG(fileName, r_screenshotJpegQuality->integer, width, height, buffer + offset, padlen);
@@ -666,7 +666,7 @@ static void R_LevelShot( void ) {
 	}
 
 	// gamma correct
-	if ( ( tr.overbrightBits > 0 ) && glConfig.deviceSupportsGamma ) {
+	if ( ( tr.overbrightBits > 0 ) && r_gammamethod->integer == GAMMA_HARDWARE) {
 		R_GammaCorrect( buffer + 18, LEVELSHOTSIZE * LEVELSHOTSIZE * 3 );
 	}
 