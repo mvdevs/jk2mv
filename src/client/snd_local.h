6d553c17746ae735b4308f5a2eecabdf0ef54b8e
ouned <michael@losert.org>
2016-07-23 18:39:40 +0200

    fix building on OS X

--- src/client/snd_local.h
+++ src/client/snd_local.h
@@ -13,8 +13,14 @@
 
 // Open AL Specific
 #ifdef USE_OPENAL
+#ifdef MACOS_X
+#	include <OpenAL/al.h>
+#	include <OpenAL/alc.h>
+#else
 #	include <AL/al.h>
 #	include <AL/alc.h>
+#endif
+
 #	include <eax.h>
 #	include <EaxMan.h>
 #endif
