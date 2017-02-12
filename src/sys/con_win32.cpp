e1f8dc757759de14144b7a1d18500fbea1bdf0a4
fau <witold.pilat@gmail.com>
2017-02-13 00:58:00 +0100

    fixup! Add const qualifier to some constant global variables

--- src/sys/con_win32.cpp
+++ src/sys/con_win32.cpp
@@ -71,7 +71,7 @@ static WORD CON_ColorCharToAttrib( char color, bool extendedColors ) {
 	}
 	else
 	{
-		float *rgba;
+		const float *rgba;
 		int colIndex = (extendedColors ? ColorIndex_Extended(color) : ColorIndex(color));
 		if ( colIndex > 7 ) colIndex = COLOR_JK2MV_FALLBACK;
 		rgba = g_color_table[ colIndex ];
