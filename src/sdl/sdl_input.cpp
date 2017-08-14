05c8e3c0e5a59095ae09c99c90ca665fbebbf131
ouned <michael@losert.org>
2017-08-14 09:05:31 +0200

    fix input of circumflex in text fields

--- src/sdl/sdl_input.cpp
+++ src/sdl/sdl_input.cpp
@@ -710,7 +710,9 @@ static void IN_ProcessEvents( void )
 
 				if (e.key.keysym.scancode == SDL_SCANCODE_GRAVE) {
 					Sys_QueEvent(0, SE_KEY, A_CONSOLE, qtrue, 0, NULL);
-					textInput = qfalse;
+					
+					if (e.key.keysym.mod & KMOD_LSHIFT || e.key.keysym.mod & KMOD_RSHIFT)
+						textInput = qfalse;
 				} else {
 					key = IN_TranslateSDLToJKKey(&e.key.keysym, qtrue);
 					if (key != A_NULL)
