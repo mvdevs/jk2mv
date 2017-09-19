a4b18019a59a8cb78315700890393b6ec160a4ac
fau <witold.pilat@gmail.com>
2017-09-19 17:35:26 +0200

    Fix s_language global symbol clashing between engine and ui module

--- src/menu/ui_main.c
+++ src/menu/ui_main.c
@@ -7316,7 +7316,7 @@ vmCvar_t	ui_recordSPDemo;
 vmCvar_t	ui_realCaptureLimit;
 vmCvar_t	ui_realWarmUp;
 vmCvar_t	ui_serverStatusTimeOut;
-vmCvar_t	s_language;
+vmCvar_t	ui_s_language;
 
 // botfilter
 vmCvar_t	ui_botfilter;
@@ -7448,7 +7448,7 @@ static const cvarTable_t cvarTable[] = {
 	{ &ui_realWarmUp, "g_warmup", "20", CVAR_ARCHIVE},
 	{ &ui_realCaptureLimit, "capturelimit", "8", CVAR_SERVERINFO | CVAR_ARCHIVE | CVAR_NORESTART},
 	{ &ui_serverStatusTimeOut, "ui_serverStatusTimeOut", "7000", CVAR_ARCHIVE},
-	{ &s_language, "s_language", "english", CVAR_ARCHIVE | CVAR_NORESTART},
+	{ &ui_s_language, "s_language", "english", CVAR_ARCHIVE | CVAR_NORESTART},
 };
 
 // bk001129 - made static to avoid aliasing
