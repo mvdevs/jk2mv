9569600a1cf160613db6d67a4e43670e78081444
fau <witold.pilat@gmail.com>
2018-01-03 13:04:43 +0100

    Fix in_nograb not releasing the mouse cursor
    Patch by Zack Middleton ioquake/ioq3@098d97bdb0cd1d23a02b9c4489766364ac31fb0c

--- src/sdl/sdl_input.cpp
+++ src/sdl/sdl_input.cpp
@@ -300,9 +300,15 @@ static void IN_ActivateMouse( void )
 		if( in_nograb->modified || !mouseActive )
 		{
 			if( in_nograb->integer )
+			{
+				SDL_SetRelativeMouseMode( SDL_FALSE );
 				SDL_SetWindowGrab( SDL_window, SDL_FALSE );
+			}
 			else
+			{
+				SDL_SetRelativeMouseMode( SDL_TRUE );
 				SDL_SetWindowGrab( SDL_window, SDL_TRUE );
+			}
 
 			in_nograb->modified = qfalse;
 		}
