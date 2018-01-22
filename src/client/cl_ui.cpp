mvdevs/jk2mv

commit 39c99368ae75b3d40fc3844fc1b7f52c294d0f3d

Prevent UI_MV_GETDLLIST SysCall from overflowing vm array

src/client/cl_ui.cpp
@@ -19,7 +19,7 @@ Ghoul2 Insert End
 extern	botlib_export_t	*botlib_export;
 void SP_Register(const char *Package);
 
-int UI_ConcatDLList(dlfile_t *files, int maxfiles);
+int UI_ConcatDLList(dlfile_t *files, const int maxfiles);
 qboolean UI_DeleteDLFile(const dlfile_t *file);
 
 vm_t *uivm;
@@ -1274,21 +1274,21 @@ Load the blacklist entrys first so they have a better chance
 to fit in the buffer
 ====================
 */
-int UI_ConcatDLList(dlfile_t *files, int maxfiles) {
+int UI_ConcatDLList(dlfile_t *files, const int maxfiles) {
 	int i, c = 0;
 
 	CL_ReadBlacklistFile();
-	for (i = 0; i < cls.downloadBlacklistLen && maxfiles; i++, maxfiles--) {
+	for (i = 0; i < cls.downloadBlacklistLen && c < maxfiles; i++, c++) {
 		Q_strncpyz(files->name, cls.downloadBlacklist[i].name, sizeof(files->name));
 		files->time = cls.downloadBlacklist[i].time;
 		files->checkksum = cls.downloadBlacklist[i].checksum;
 		files->blacklisted = qtrue;
 
-		c++; files++;
+		files++;
 	}
 	CL_BlacklistWriteCloseFile();
 
-	return c + FS_GetDLList(files, maxfiles);
+	return c + FS_GetDLList(files, c);
 }
 
 qboolean UI_DeleteDLFile(const dlfile_t *file) {

src/qcommon/files.cpp
@@ -3905,9 +3905,9 @@ void FS_FilenameCompletion( const char *dir, const char *ext, qboolean stripExt,
 		FS_FilenameCompletion(dir, pch + 1, stripExt, callback);
 }
 
-int FS_GetDLList(dlfile_t *files, int maxfiles) {
+int FS_GetDLList(dlfile_t *files, const int maxfiles) {
 	const char **dirs, **pakfiles;
-	int c, d, paknum, dirnum;
+	int c, d, paknum, dirnum, filesnum;
 	char *gamepath;
 	int ret = 0;
 
@@ -3917,7 +3917,7 @@ int FS_GetDLList(dlfile_t *files, int maxfiles) {
 		return qfalse;
 	}
 
-	for (d = 0; d < dirnum; d++) {
+	for (filesnum = 0, d = 0; d < dirnum; d++) {
 		if (!strcmp(dirs[d], ".") || !strcmp(dirs[d], "..")) {
 			continue;
 		}
@@ -3928,7 +3928,7 @@ int FS_GetDLList(dlfile_t *files, int maxfiles) {
 			continue;
 		}
 
-		for (c = 0; c < paknum && maxfiles; c++) {
+		for (c = 0; c < paknum && filesnum < maxfiles; c++, filesnum++) {
 			if (Q_stricmpn(pakfiles[c], "dl_", 3)) {
 				continue;
 			}
@@ -3937,7 +3937,7 @@ int FS_GetDLList(dlfile_t *files, int maxfiles) {
 			files->blacklisted = qfalse;
 			files->time = 0;
 
-			files++; ret++; maxfiles--;
+			files++; ret++;
 		}
 
 		Sys_FreeFileList(pakfiles);

commit ea76e40ec143177911555a8cc712e1fbe3030d45

fix download list in settings menu bug introduced in 39c99368ae75b3d40fc3844fc1b7f52c294d0f3d

src/client/cl_ui.cpp
@@ -1297,10 +1297,10 @@ to fit in the buffer
 ====================
 */
 int UI_ConcatDLList(dlfile_t *files, const int maxfiles) {
-	int i, c = 0;
+	int i;
 
 	CL_ReadBlacklistFile();
-	for (i = 0; i < cls.downloadBlacklistLen && c < maxfiles; i++, c++) {
+	for (i = 0; i < cls.downloadBlacklistLen && i < maxfiles; i++) {
 		Q_strncpyz(files->name, cls.downloadBlacklist[i].name, sizeof(files->name));
 		files->time = cls.downloadBlacklist[i].time;
 		files->checkksum = cls.downloadBlacklist[i].checksum;
@@ -1310,7 +1310,7 @@ int UI_ConcatDLList(dlfile_t *files, const int maxfiles) {
 	}
 	CL_BlacklistWriteCloseFile();
 
-	return c + FS_GetDLList(files, c);
+	return i + FS_GetDLList(files, maxfiles - i);
 }
 
 qboolean UI_DeleteDLFile(const dlfile_t *file) {