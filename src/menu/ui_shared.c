308248e6c6351cd39932132d8ccf54594b7753a9
fau <witold.pilat@gmail.com>
2018-06-03 16:13:39 +0200

    Fix Clang warnings

--- src/menu/ui_shared.c
+++ src/menu/ui_shared.c
@@ -6588,7 +6588,6 @@ static void Item_TextScroll_BuildLines ( itemDef_t* item )
 					// Special case, don't consider line breaking if you're on an asian punctuation char of
 					//	a language that doesn't use spaces...
 					//
-					uiLetter = uiLetter;	// breakpoint line only
 				}
 				else
 				{
