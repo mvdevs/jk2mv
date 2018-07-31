e9ecbf6e3912dc9d4f6e20080b0db135b9210516
ouned <michael@losert.org>
2018-07-31 21:35:43 +0200

    add appimage build script

---
+++ build/build-appimage.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+type cmake >/dev/null 2>&1 || { echo >&2 "Can't find cmake."; exit 1; }
+
+BUILD_DIR="`uname`-`uname -m`-appimage"
+mkdir "$BUILD_DIR"
+cd "$BUILD_DIR"
+
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DUseInternalLibs=OFF -DBuildPortableVersion=OFF -DBuildMVDED=OFF ../..
+make
+make install DESTDIR=vinstall
+
+wget -q https://raw.githubusercontent.com/AppImage/AppImages/9249a99e653272416c8ee8f42cecdde12573ba3e/functions.sh -O ./functions.sh
+. ./functions.sh
+
+APP="jk2mv"
+LOWERAPP="org.mvdevs.jk2mv"
+VERSION=$(git describe --tags)
+ARCH=$(uname -m)
+
+rm -r $APP.AppDir
+mkdir $APP.AppDir
+cd $APP.AppDir
+
+########################################################################
+# Copy install output
+########################################################################
+cp -a ../vinstall/* .
+mv ./usr/local/* ./usr
+rm -r ./usr/local
+
+########################################################################
+# Copy desktop and icon file to AppDir for AppRun to pick them up
+########################################################################
+
+get_apprun
+get_desktop
+get_icon
+
+########################################################################
+# Copy in the dependencies that cannot be assumed to be available
+# on all target systems
+########################################################################
+
+copy_deps
+
+move_lib
+find ./usr/lib/* -name '*.so*' -exec mv -it ./usr/lib {} +
+find ./usr/lib -type d -empty -delete
+
+########################################################################
+# Delete stuff that should not go into the AppImage
+########################################################################
+
+# https://github.com/probonopd/AppImages/blob/master/excludelist
+delete_blacklisted
+
+rm ./usr/lib/libpulse* # pulseaudio libs should be provided by the os
+
+########################################################################
+# desktopintegration asks the user on first run to install a menu item
+########################################################################
+
+get_desktopintegration $LOWERAPP
+
+########################################################################
+# AppDir complete
+# Now packaging it as an AppImage
+########################################################################
+
+cd ..
+generate_type2_appimage
+mv ../out/*.AppImage out/
+mv out/*.AppImage out/jk2mv-v$VERSION-linux-$ARCH.AppImage
+
