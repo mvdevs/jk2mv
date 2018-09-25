06cd3a5c434606f9b50cf25d0ae00f1ab5f101ef
fau <witold.pilat@gmail.com>
2018-09-25 23:09:42 +0200

    Fix map autocomplete with pk3s using \ path separators

--- src/qcommon/files.cpp
+++ src/qcommon/files.cpp
@@ -2279,7 +2279,8 @@ static const char **FS_ListFilteredFiles( const char *path, const char *extensio
 						continue;
 					}
 
-					if ( !compat && name[pathLength] != '/' ) {
+					// there can be either \ or / path separators in a pk3 file
+					if ( !compat && name[pathLength] != '/' && name[pathLength] != '\\' ) {
 						continue;
 					}
 
