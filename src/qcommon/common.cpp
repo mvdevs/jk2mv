f5f3c1a7ced9b7d732dcb44b7cfa3c31b4da9620
Bucky21659 <mjmiller98@gmail.com>
2018-11-07 15:28:01 -0700

    Add "exit" command
    arbitrary

--- src/qcommon/common.cpp
+++ src/qcommon/common.cpp
@@ -2540,6 +2540,7 @@ void Com_Init( char *commandLine ) {
 		Cmd_AddCommand ("freeze", Com_Freeze_f);
 	}
 	Cmd_AddCommand ("quit", Com_Quit_f);
+	Cmd_AddCommand ("exit", Com_Quit_f);
 	Cmd_AddCommand ("changeVectors", MSG_ReportChangeVectors_f );
 	Cmd_AddCommand ("writeconfig", Com_WriteConfig_f );
 	Cmd_SetCommandCompletionFunc( "writeconfig", Cmd_CompleteCfgName );
