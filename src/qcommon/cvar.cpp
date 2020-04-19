cd9e22f50c509a351c55f4a4859ab527dc2ffbd4
Daggolin <Daggolin@Gmx.de>
2020-04-19 20:03:31 +0200

    Don't error when a cvar value exceeds the maximum length for a vmCvar_t. Just print a message and cut off the rest.

--- src/qcommon/cvar.cpp
+++ src/qcommon/cvar.cpp
@@ -1096,7 +1096,7 @@ void	Cvar_Update( vmCvar_t *vmCvar ) {
 	vmCvar->modificationCount = cv->modificationCount;
 	// bk001129 - mismatches.
 	if ( strlen(cv->string)+1 > MAX_CVAR_VALUE_STRING )
-	  Com_Error( ERR_DROP, "Cvar_Update: src %s length %d exceeds MAX_CVAR_VALUE_STRING",
+	  Com_Printf( "Cvar_Update: src \"%s\" length %d exceeds MAX_CVAR_VALUE_STRING\n",
 		  cv->string,
 		  (int)strlen(cv->string) );
 	// bk001212 - Q_strncpyz guarantees zero padding and dest[MAX_CVAR_VALUE_STRING-1]==0
