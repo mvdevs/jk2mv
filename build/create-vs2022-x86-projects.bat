2ca5d944ac15393affaa0c66c8c3108ed6671afe
Daggolin <Daggolin@Gmx.de>
2023-01-22 00:28:43 +0100

    Add vs2022 CMake generator scripts.

---
+++ build/create-vs2022-x86-projects.bat
@@ -0,0 +1,17 @@
+@REM Create JK2MV projects for Visual Studio 2022 using CMake
+@echo off
+for %%X in (cmake.exe) do (set FOUND=%%~$PATH:X)
+if not defined FOUND (
+	echo CMake was not found on your system. Please make sure you have installed CMake
+	echo from http://www.cmake.org/ and cmake.exe is installed to your system's PATH
+	echo environment variable.
+	echo.
+	pause
+	exit /b 1
+) else (
+	echo Found CMake!
+)
+mkdir msvc17_x86
+cd msvc17_x86
+cmake -G "Visual Studio 17" -A "Win32" ../..
+pause
