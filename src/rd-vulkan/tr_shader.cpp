3aa607f246842786f32e6db7f8a1eab65f75e00e
tayst <taylor.brough13@gmail.com>
2026-02-28 21:34:24 +0800

    Vulkan: Fix clang warning: warning: address of array st1->lightmapStyle will always evaluate to true

--- src/rd-vulkan/tr_shader.cpp
+++ src/rd-vulkan/tr_shader.cpp
@@ -3792,7 +3792,7 @@ static int CollapseMultitexture( unsigned int st0bits, shaderStage_t *st0, shade
 	}
 
 	// preserve lightmap style
-	if (st1->lightmapStyle)
+	if (st1->lightmapStyle[0])
 	{
 		st0->lightmapStyle[1] = st1->lightmapStyle[0];
 	}
