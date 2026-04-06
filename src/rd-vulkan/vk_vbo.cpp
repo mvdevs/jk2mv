8eef2ba1866c02c569631ebccaf8bd3ec3d99caf
JKSunny <60109631+JKSunny@users.noreply.github.com>
2026-04-06 12:40:55 +0200

    Vulkan: Add an early return in R_BuildMD3 to skip when r_vbo_models is not enabled

--- src/rd-vulkan/vk_vbo.cpp
+++ src/rd-vulkan/vk_vbo.cpp
@@ -934,7 +934,7 @@ int get_mdv_stride( void ) {
 
 void R_BuildMDXM( model_t *mod, mdxmHeader_t *mdxm )
 {
-	if( !vk.vboGhoul2Active )
+	if ( !vk.vboGhoul2Active )
 		return;
 
 	mdxmVBOModel_t		*vboModel;
@@ -1174,6 +1174,9 @@ void R_BuildMDXM( model_t *mod, mdxmHeader_t *mdxm )
 
 void R_BuildMD3( model_t *mod, mdvModel_t *mdvModel ) 
 {
+	if ( !vk.vboMdvActive )
+		return;
+
 	mdvVertex_t    *v;
 	mdvSt_t        *st;
 	mdvSurface_t   *surf;
